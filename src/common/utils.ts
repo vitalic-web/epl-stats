b2c3903dbe579842652c40725c61e14c63258b43
Vitaly <vitalic.web@gmail.com>
2022-04-29 17:17:05 +0200

    Add method for dates

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,12 +1,37 @@
+import { DateTime } from 'luxon';
+import { WeekDates } from '@/common/types';
+import { LocationQueryValue } from 'vue-router';
+
 const getSeasonYears = (start: string, end: string) => {
   const startDate = new Date(start);
   const endDate = new Date(end);
   return `${startDate.getFullYear()} - ${endDate.getFullYear()}`;
 };
 
-const test = () => console.log('test');
+const toDate = (value: LocationQueryValue | LocationQueryValue[]) => new Date(String(value));
+
+const getDatesForQuery = (dates: Date[]) => {
+  const startDate = DateTime.fromJSDate(dates[0]).toISODate();
+  const endDate = DateTime.fromJSDate(dates[1]).toISODate();
+
+  return {
+    startDate,
+    endDate,
+  };
+};
+
+const getCurrentWeekDates = (): WeekDates => {
+  const currentDate = DateTime.now();
+  const nextWeekDate = currentDate.plus({ days: 6 });
+  return {
+    startDate: currentDate.toISODate(),
+    endDate: nextWeekDate.toISODate(),
+  };
+};
 
 export {
   getSeasonYears,
-  test,
+  toDate,
+  getDatesForQuery,
+  getCurrentWeekDates,
 };
