4e16f674a01a11533cfd420c4ce0428f79e65ae7
Vitaly <vitalic.web@gmail.com>
2022-05-01 15:41:35 +0200

    Create match team component

---
+++ src/components/MatchInfo/MatchTeam.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="match-team">
+    <img class="match-team__img" :src="team.crestUrl" :alt="team.name">
+    <div class="match-team__name">{{ team.name }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  team: Object,
+});
+</script>
+
+<style lang="scss">
+.match-team {
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  &__img {
+    width: 50px;
+  }
+  &__name {
+    margin-left: 10px;
+  }
+}
+</style>
