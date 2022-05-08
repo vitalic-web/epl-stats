6962dfe179ea8d6c47a67e750404dc80d4d89e0e
Vitaly <vitalic.web@gmail.com>
2022-05-08 16:14:40 +0200

    Update breadcrumb

--- src/components/BreadcrumbNav.vue
+++ src/components/BreadcrumbNav.vue
@@ -1,33 +1,27 @@
 <template>
-<!--  <el-breadcrumb :separator-icon="ArrowRight">-->
-<!--    <el-breadcrumb-item v-for="item in navItems" :key="item.name">-->
-<!--      <a v-if="item.path" :href="item.path">{{ item.name }}</a>-->
-<!--      <span v-else>{{ item.name }}</span>-->
-<!--    </el-breadcrumb-item>-->
-<!--  </el-breadcrumb>-->
-  <el-breadcrumb :separator-icon="ArrowRight">
+  <el-breadcrumb v-if="name" :separator-icon="ArrowRight">
+    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+    <el-breadcrumb-item
+      :to="{ path: route.meta.baseUrl }"
+    >{{ route.meta.baseUrlName }}</el-breadcrumb-item>
+    <el-breadcrumb-item>{{ name }}</el-breadcrumb-item>
+  </el-breadcrumb>
+  <el-breadcrumb v-else :separator-icon="ArrowRight">
     <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
     <el-breadcrumb-item>{{ route.name }}</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
 import { useRoute } from 'vue-router';
 import { ArrowRight } from '@element-plus/icons-vue';
-import { Breadcrumb } from '@/common/types';
 
 const route = useRoute();
 
-// TODO: add isParams
-
-// defineProps({
-//   navItems: {
-//     type: Array as PropType<Breadcrumb>,
-//     required: true,
-//   },
-// });
-
-console.log('route', route);
-console.log('route.params', Object.keys(route.params).length);
+defineProps({
+  name: {
+    type: String,
+    default: null,
+  },
+});
 </script>
