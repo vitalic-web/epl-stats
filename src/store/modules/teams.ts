afc5bf628dadf1a4e5f36ca6567ab64d4b794470
Vitaly <vitalic.web@gmail.com>
2022-05-14 16:55:56 +0200

    Add selecteadTeam to teams module

--- src/store/modules/teams.ts
+++ src/store/modules/teams.ts
@@ -5,10 +5,28 @@ import EventService from '@/services/EventService';
 const teams: Module<Teams, RootState> = {
   state: () => ({
     allTeams: [],
+    selectedTeam: {
+      name: '',
+      founded: 0,
+      phone: '',
+      venue: '',
+      clubColors: '',
+      address: '',
+      website: '',
+      squad: {
+        name: '',
+        position: '',
+        dateOfBirth: '',
+        nationality: '',
+      },
+    },
   }),
   mutations: {
     SET_ALL_TEAMS(state, teamsData) {
       state.allTeams = teamsData;
+    },
+    SET_SELECTED_TEAM(state, teamData) {
+      state.selectedTeam = teamData;
     },
   },
   actions: {
@@ -17,6 +35,17 @@ const teams: Module<Teams, RootState> = {
         commit('SET_LOADING_STATUS', 'loading');
         const teamsData = await EventService.getTeams();
         commit('SET_ALL_TEAMS', teamsData.data.teams);
+      } catch (error) {
+        console.log('error');
+      } finally {
+        commit('SET_LOADING_STATUS', 'notLoading');
+      }
+    },
+    async fetchTeamInfo({ commit }, teamID: string) {
+      try {
+        commit('SET_LOADING_STATUS', 'loading');
+        const selectedTeam = await EventService.getTeamInfo(teamID);
+        commit('SET_SELECTED_TEAM', selectedTeam.data);
       } catch (error) {
         console.log('error');
       } finally {
