a6cfd6346cae74bce62dc1e1dd20351a4c5dffbd
Vitaly <vitalic.web@gmail.com>
2022-05-15 13:15:30 +0200

    Add stats module to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,59 +1,23 @@
 import { createStore } from 'vuex';
 import common from '@/store/modules/common';
 import teams from '@/store/modules/teams';
+import stats from '@/store/modules/stats';
 
 // TODO: add vuex modules
-// teams module (teams, team info)
 // stats module (table, scorers, winners)
 // matches module (matches)
 export default createStore({
   modules: {
     common,
     teams,
+    stats,
   },
   // state: {
-  //   selectedTeam: {
-  //     name: '',
-  //     founded: 0,
-  //     phone: '',
-  //     venue: '',
-  //     clubColors: '',
-  //     address: '',
-  //     website: '',
-  //     squad: {
-  //       name: '',
-  //       position: '',
-  //       dateOfBirth: '',
-  //       nationality: '',
-  //     },
-  //   },
-  //   table: {
-  //     season: {
-  //       startDate: '',
-  //       endDate: '',
-  //       currentMatchday: 0,
-  //     },
-  //     standings: [],
-  //   },
   //   scorers: [],
   //   winners: [],
   //   matches: [],
   // },
-  // getters: {
-  //   seasonDate(state) {
-  //     return getSeasonYears(state.table.season.startDate, state.table.season.endDate);
-  //   },
-  // },
   // mutations: {
-  //   SET_SELECTED_TEAM(state, team) {
-  //     state.selectedTeam = team;
-  //   },
-  //   SET_TABLE_SEASON(state, season) {
-  //     state.table.season = season;
-  //   },
-  //   SET_TABLE_STANDINGS(state, standings) {
-  //     state.table.standings = standings;
-  //   },
   //   SET_SCORERS(state, scorers) {
   //     state.scorers = scorers;
   //   },
@@ -65,29 +29,6 @@ export default createStore({
   //   },
   // },
   // actions: {
-  //   async fetchTeamInfo({ commit }, teamID: string) {
-  //     try {
-  //       commit('SET_LOADING_STATUS', 'loading');
-  //       const selectedTeam = await EventService.getTeamInfo(teamID);
-  //       commit('SET_SELECTED_TEAM', selectedTeam.data);
-  //     } catch (error) {
-  //       console.log('error');
-  //     } finally {
-  //       commit('SET_LOADING_STATUS', 'notLoading');
-  //     }
-  //   },
-  //   async fetchTable({ commit }) {
-  //     try {
-  //       commit('SET_LOADING_STATUS', 'loading');
-  //       const tableData = await EventService.getTable();
-  //       commit('SET_TABLE_SEASON', tableData.data.season);
-  //       commit('SET_TABLE_STANDINGS', tableData.data.standings[0].table);
-  //     } catch (error) {
-  //       console.log('error');
-  //     } finally {
-  //       commit('SET_LOADING_STATUS', 'notLoading');
-  //     }
-  //   },
   //   async fetchScorers({ commit }) {
   //     try {
   //       commit('SET_LOADING_STATUS', 'loading');
