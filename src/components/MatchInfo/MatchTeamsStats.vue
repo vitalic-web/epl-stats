f12812214b9312babd59461ed7f2e191550f115d
Vitaly <vitalic.web@gmail.com>
2022-05-16 17:32:44 +0200

    Create MatchTeamsStats component

---
+++ src/components/MatchInfo/MatchTeamsStats.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="teams-stats">MatchTeamsStats</div>
+  <div>{{ teamsStats }}</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  teamsStats: Object,
+});
+</script>
+
+<style lang="scss">
+.teams-stats {
+  margin-top: 10px;
+  border: 1px solid red;
+  width: 97%;
+}
+</style>
