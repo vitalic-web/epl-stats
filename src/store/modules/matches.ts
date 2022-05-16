93fc7a9409d96db65afcfaaec34f63814b9d1a43
Vitaly <vitalic.web@gmail.com>
2022-05-16 17:34:03 +0200

    Add teamsStats state to matches module

--- src/store/modules/matches.ts
+++ src/store/modules/matches.ts
@@ -8,10 +8,35 @@ import EventService from '@/services/EventService';
 const teams: Module<Matches, RootState> = {
   state: () => ({
     allMatches: [],
+    teamsStats: {
+      numberOfMatches: 0,
+      totalGoals: 0,
+      homeTeam: {
+        id: 0,
+        name: '',
+        wins: 0,
+        draws: 0,
+        losses: 0,
+      },
+      awayTeam: {
+        id: 0,
+        name: '',
+        wins: 0,
+        draws: 0,
+        losses: 0,
+      },
+    },
+    isLoading: false,
   }),
   mutations: {
     SET_MATCHES(state, matchesData) {
       state.allMatches = matchesData;
+    },
+    SET_TEAMS_STATS(state, teamsStatsData) {
+      state.teamsStats = teamsStatsData;
+    },
+    SET_IS_LOADING(state, isLoadingStatus) {
+      state.isLoading = isLoadingStatus;
     },
   },
   actions: {
@@ -32,6 +57,19 @@ const teams: Module<Matches, RootState> = {
         console.log('error');
       } finally {
         commit('SET_LOADING_STATUS', 'notLoading');
+      }
+    },
+    async fetchMatchTeamsStats({ commit }, id: number) {
+      console.log('is fetchMatchTeamsStats ??????');
+      try {
+        commit('SET_IS_LOADING', true);
+        const teamsStats = await EventService.getMatchTeamsStats(id);
+        console.log('teamsStats.data.head2head', teamsStats.data.head2head);
+        commit('SET_TEAMS_STATS', teamsStats.data.head2head);
+      } catch (error) {
+        console.log('error');
+      } finally {
+        commit('SET_IS_LOADING', false);
       }
     },
   },
