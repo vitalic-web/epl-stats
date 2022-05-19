64e9a0c6c9cc1a363fb805cb71533fb0e46228aa
Vitaly <vitalic.web@gmail.com>
2022-05-19 18:27:20 +0200

    Add styles to match teams stats

--- src/components/MatchInfo/MatchTeamsStats.vue
+++ src/components/MatchInfo/MatchTeamsStats.vue
@@ -1,19 +1,26 @@
 <template>
-<!--  TODO: fix display same info in all matches -->
   <div class="teams-stats">
-    <div>number of matches: {{ teamsStats.numberOfMatches }}</div>
-    <div>total goals: {{ teamsStats.totalGoals }}</div>
-    <div>
-      {{ teamsStats.homeTeam.name }}:
-      wins {{ teamsStats.homeTeam.wins }},
-      draws {{ teamsStats.homeTeam.draws }},
-      losses {{ teamsStats.homeTeam.losses }}
+    <div class="teams-stats__item">
+      <span>number of matches:</span><span>{{ teamsStats.numberOfMatches }}</span>
     </div>
-    <div>
-      {{ teamsStats.awayTeam.name }}:
-      wins {{ teamsStats.awayTeam.wins }},
-      draws {{ teamsStats.awayTeam.draws }},
-      losses {{ teamsStats.awayTeam.losses }}
+    <div class="teams-stats__item">
+      <span>total goals:</span><span>{{ teamsStats.totalGoals }}</span>
+    </div>
+    <div class="teams-stats__item">
+      <span class="teams-stats__club-name">{{ teamsStats.homeTeam.name }}:</span>
+      <span>
+        wins {{ teamsStats.homeTeam.wins }},
+        draws {{ teamsStats.homeTeam.draws }},
+        losses {{ teamsStats.homeTeam.losses }}
+      </span>
+    </div>
+    <div class="teams-stats__item">
+      <span class="teams-stats__club-name">{{ teamsStats.awayTeam.name }}:</span>
+      <span>
+        wins {{ teamsStats.awayTeam.wins }},
+        draws {{ teamsStats.awayTeam.draws }},
+        losses {{ teamsStats.awayTeam.losses }}
+      </span>
     </div>
   </div>
 </template>
@@ -28,5 +35,17 @@ defineProps({
 .teams-stats {
   margin-top: 10px;
   width: 97%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  font-size: 14px;
+  &__item {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+  &__club-name {
+    text-decoration: underline;
+  }
 }
 </style>
