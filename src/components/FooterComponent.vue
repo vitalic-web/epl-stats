0f21812ef1625e59bfb043a2955290a87afc41b8
Vitaly <vitalic.web@gmail.com>
2022-05-20 17:14:40 +0200

    Add github link to footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,27 @@
 <template>
   <footer class="footer">
     <div class="footer__name">
-      <img class="footer__logo" src="../assets/epl-logo-main.png" alt="English Premier League Logo">
+      <img
+        class="footer__logo"
+        src="../assets/epl-logo-main.png"
+        alt="English Premier League Logo"
+      >
       English Premier League Stats
     </div>
-    <div class="footer__author">&#169; vitalic-web</div>
+    <div class="footer__author">
+      <span class="footer__copy-right">&#169;</span>
+      <span>vitalic-web</span>
+      <div class="footer__github">
+        <img class="footer__github-logo" src="../assets/GitHub-logo.png" alt="GitHub logo">
+        <a
+          class="footer__github-link"
+          href="https://github.com/vitalic-web"
+          target="_blank"
+        >
+          github.com/vitalic-web
+        </a>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -19,17 +36,37 @@
   display: flex;
   justify-content: space-between;
   border-top: 1px solid black;
-  &__name {
-    padding-left: 10px;
-    display: flex;
-  }
-  &__author {
-    padding-right: 10px;
-  }
   &__logo {
     width: 20px;
     height: 20px;
     margin-right: 5px;
+  }
+  &__name {
+    padding-left: 10px;
+    display: flex;
+    align-items: center;
+  }
+  &__copy-right {
+    font-size: 24px;
+    margin-right: 5px;
+  }
+  &__author {
+    padding-right: 10px;
+    display: flex;
+    align-items: center;
+  }
+  &__github {
+    display: flex;
+    align-items: center;
+  }
+  &__github-logo {
+    width: 20px;
+    height: 20px;
+    margin: 0 5px 0 15px;
+  }
+  &__github-link {
+    color: black;
+    text-decoration: none;
   }
 }
 </style>
