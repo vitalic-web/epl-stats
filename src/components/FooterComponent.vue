36285054b9b1a28d5f2066b9d5883ccf3aae8b52
Vitaly <vitalic.web@gmail.com>
2022-05-20 15:57:58 +0200

    Update footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,17 +1,35 @@
 <template>
   <footer class="footer">
-    <p>{{ name }}</p>
+    <div class="footer__name">
+      <img class="footer__logo" src="../assets/epl-logo-main.png" alt="English Premier League Logo">
+      English Premier League Stats
+    </div>
+    <div class="footer__author">&#169; vitalic-web</div>
   </footer>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const name = ref('Footer');
 </script>
 
 <style lang="scss">
 .footer {
+  width: 100%;
   margin-top: auto;
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid black;
+  &__name {
+    padding-left: 10px;
+    display: flex;
+  }
+  &__author {
+    padding-right: 10px;
+  }
+  &__logo {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+  }
 }
 </style>
