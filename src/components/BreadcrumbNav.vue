4ceb2883c44cb564aa8929833e0ae9ed3418a4aa
Vitaly <vitalic.web@gmail.com>
2022-05-22 15:11:08 +0200

    Fix breadcrumb styles

--- src/components/BreadcrumbNav.vue
+++ src/components/BreadcrumbNav.vue
@@ -1,15 +1,17 @@
 <template>
-  <el-breadcrumb v-if="name" :separator-icon="ArrowRight">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item
-      :to="{ path: route.meta.baseUrl }"
-    >{{ route.meta.baseUrlName }}</el-breadcrumb-item>
-    <el-breadcrumb-item>{{ name }}</el-breadcrumb-item>
-  </el-breadcrumb>
-  <el-breadcrumb v-else :separator-icon="ArrowRight">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item>{{ route.name }}</el-breadcrumb-item>
-  </el-breadcrumb>
+  <div class="breadcrumb-nav">
+    <el-breadcrumb v-if="name" :separator-icon="ArrowRight">
+      <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+      <el-breadcrumb-item
+        :to="{ path: route.meta.baseUrl }"
+      >{{ route.meta.baseUrlName }}</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ name }}</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-breadcrumb v-else :separator-icon="ArrowRight">
+      <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ route.name }}</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,3 +27,12 @@ defineProps({
   },
 });
 </script>
+
+<style lang="scss">
+.breadcrumb-nav {
+  margin-top: 10px;
+  width: 95%;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
