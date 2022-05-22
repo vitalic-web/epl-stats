206f734699782669cdc3ba44d8fb0d8932af494c
Vitaly <vitalic.web@gmail.com>
2022-05-22 15:00:01 +0200

    Display header on all views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
+  <HeaderComponent />
   <router-view/>
 </template>
+
+<script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue';
+</script>
 
 <style lang="scss">
 #app {
