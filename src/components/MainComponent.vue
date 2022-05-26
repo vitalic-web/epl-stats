1eba9818759c0e85d9eea1ae50807e0619c3139d
Vitaly <vitalic.web@gmail.com>
2022-05-26 13:11:57 +0200

    Add all page links to main page

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,14 +1,20 @@
 <template>
   <main class="main">
     <PageLink name="Teams" route-name="teams" img-path="src/assets/top6.png">
-      <img class="main__img" src="../assets/top6.png" alt="teams">
+      <img class="main__img" src="../assets/epl-big-6.jpeg" alt="teams">
     </PageLink>
-<!--    <PageLink name="Table" route-name="table" />-->
+    <PageLink name="Table" route-name="table">
+      <img class="main__img" src="../assets/epl-clubs-min-scaled.webp" alt="table">
+    </PageLink>
     <PageLink name="Scorers" route-name="scorers">
       <img class="main__img" src="../assets/epl-scorers.jpeg" alt="scorers">
     </PageLink>
-<!--    <PageLink name="Winners" route-name="winners" />-->
-<!--    <PageLink name="Matches" route-name="matches" :query="weekDates" />-->
+    <PageLink name="Winners" route-name="winners">
+      <img class="main__img" src="../assets/epl-winners.jpeg" alt="winners">
+    </PageLink>
+    <PageLink name="Matches" route-name="matches" :query="weekDates">
+      <img class="main__img" src="../assets/epl-matches.jpeg" alt="matches">
+    </PageLink>
   </main>
 </template>
 
@@ -21,13 +27,16 @@ const weekDates = getCurrentWeekDates();
 
 <style lang="scss">
 .main {
-  display: flex;
-  flex-direction: column;
   margin-top: 30px;
   width: 95%;
+  display: grid;
+  gap: 15px;
+  grid-template-columns: repeat(2, 1fr);
   &__img {
-    height: 300px;
-    object-fit: contain;
+    height: 235px;
+    width: 65%;
+    object-fit: cover;
+    object-position: top;
   }
 }
 </style>
